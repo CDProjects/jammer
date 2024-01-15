7fc4c280d79e80e360d06e5d779c0f66bc697280
CDProjects <CDProjects@gmail.com>
2024-01-15 15:01:49 +0200

    updatePlaylistName function implemented, onNameChange prop implemented

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -4,17 +4,19 @@ import './Playlist.css';
 
 function Playlist({ playlistName, playlistTracks, onNameChange, onRemove, onSave }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [newPlaylistName, setNewPlaylistName] = useState(playlistName);
 
   const handleNameClick = () => {
     setIsEditing(true);
   };
 
   const handleNameChange = (e) => {
-    onNameChange(e.target.value);
+    setNewPlaylistName(e.target.value);
   };
 
   const handleNameBlur = () => {
     setIsEditing(false);
+    onNameChange(newPlaylistName); // Call the prop function to update the playlist name
   };
 
   return (
@@ -22,7 +24,7 @@ function Playlist({ playlistName, playlistTracks, onNameChange, onRemove, onSave
       {isEditing ? (
         <input 
           className="Playlist-input" 
-          defaultValue={playlistName} 
+          value={newPlaylistName} 
           onChange={handleNameChange} 
           onBlur={handleNameBlur} 
           autoFocus
