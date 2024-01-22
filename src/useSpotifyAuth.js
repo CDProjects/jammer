c2e55ce6d54662e8c6255db0642010b444efde6a
CDProjects <CDProjects@gmail.com>
2024-01-22 20:16:24 +0200

    Local storage access token implemented to resolve redirect loop

--- src/useSpotifyAuth.js
+++ src/useSpotifyAuth.js
@@ -6,35 +6,29 @@ const scopes = ['playlist-modify-public', 'playlist-modify-private'];
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 const useSpotifyAuth = () => {
-  const [accessToken, setAccessToken] = useState(null);
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('spotifyAccessToken'));
 
   useEffect(() => {
-    console.log('useEffect triggered in useSpotifyAuth');
+    if (!accessToken) {
+      const accessTokenMatch = window.location.hash.match(/#access_token=([^&]*)/);
+      const expiresInMatch = window.location.hash.match(/expires_in=([^&]*)/);
 
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+      if (accessTokenMatch && expiresInMatch) {
+        const token = accessTokenMatch[1];
+        const expiresIn = Number(expiresInMatch[1]);
 
-    console.log('Access Token Match:', accessTokenMatch);
-    console.log('Expires In Match:', expiresInMatch);
+        localStorage.setItem('spotifyAccessToken', token);
+        setAccessToken(token);
 
-    if (accessTokenMatch && expiresInMatch) {
-      const token = accessTokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
+        window.setTimeout(() => {
+          localStorage.removeItem('spotifyAccessToken');
+          setAccessToken(null);
+        }, expiresIn * 1000);
 
-      setAccessToken(token);
-      console.log('Token set:', token);
-
-      window.setTimeout(() => {
-        console.log('Token expired, clearing access token');
-        setAccessToken(null)
-      }, expiresIn * 1000);
-
-      window.history.pushState('Access Token', null, '/');
-      console.log('URL parameters cleared');
-    } else if (!accessToken) {
-      console.log('Redirecting to Spotify login');
-      const url = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(' ')}&response_type=token&show_dialog=true`;
-      window.location = url;
+        window.history.pushState('Access Token', null, '/');
+      } else if (!window.location.hash) {
+        window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(' ')}&response_type=token&show_dialog=true`;
+      }
     }
   }, [accessToken]);
 
